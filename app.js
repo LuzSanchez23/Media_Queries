eb50a99bed1b03dab49bebc9a18403e2964dc457
Luz Sanchez <nena_baby23@hotmail.com>
2021-10-26 20:14:42 -0600

    adding js

---
+++ app.js
@@ -0,0 +1,66 @@
+console.log('The Fortune Teller');
+
+var children= 3;
+var firstName= "Oscar";
+var place= "Paris";
+var job="secretary";
+
+    alert("You will be a" + " " + job + " " + "In" + " "  + place  + '\n' + 
+     "and married to" + " " + firstName + " " + "with" + " " + children + " " + "kids");
+
+    //  document.write("You will be a" + " " + job + " " + "In" + " "  + place  + '\n' + 
+    //  "and married to" + " " + firstName + " " + "with" + " " + children + " " + "kids";)
+
+    //  document.getElementById("demo").innerHTML = "You will be a" + " " + job + " " + "In" + " "  + place  + '\n' + 
+    //  "and married to" + " " + firstName + " " + "with" + " " + children + " " + "kids";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log('The Fortune Teller');
+// //window Objects-abajo//
+
+// //alerts message on the page//
+// alert ("You have to be 18 to enter this site");
+// //ask a yes or no//
+// confirm ("Are you over 18?");
+// //give an imput box or prompys an answer//
+// // prompt ("How old are you?");
+
+// console.log("hello" + "world");
+// console.log("Hello + 5*10");
+// console.log("hello" + 5*10);
+
+// var firstName= "April";
+// var lastName= "Long";
+// var middleInitial= "R";
+
+// //console.log, alert, document.write or we can target html element//
+// console.log(firstName[4]);
+
+// console.log("My name is" + " " + firstName + '\n' + 
+//     "My last name is" + " " + lastName  + '\n' + 
+//      "My last name is" + " " + lastName);
+
+//      document.write("My name is" + " " + firstName + '\n' + 
+//     "My last name is" + " " + lastName  + '\n' + 
+//      "My last name is" + " " + lastName);
+
+//      alert("My name is" + " " + firstName + '\n' + 
+//     "My last name is" + " " + lastName  + '\n' + 
+//      "My last name is" + " " + lastName);
+
+//      document.getElementById("demo").innerHTML="My name is" + " " + firstName + '\n' + 
+//      "My last name is" + " " + lastName  + '\n' + 
+//       "My last name is" + " " + lastName;
+     
